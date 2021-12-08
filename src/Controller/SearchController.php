f4d9817a6ff53a4baa00cd48b4fa5c2ab1b55136
Sayn <marvincrepin@gmail.com>
2021-12-08 13:12:07 +0100

    SearchController: List by Location spaces (simple version)

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -17,25 +17,34 @@ class SearchController extends AbstractController
      * @Route("/", name="app_search")
      */
 
-    public function index(Request $request, SpaceRepository $spaceRepository): Response
+    public function index(Request $request): Response
     {
-        // ...
 
-        $form = $this->createForm(SearchType::class, [
-            'method' => 'GET',
-        ]);
+        $form = $this->createForm(SearchType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $location = $form->get('location')->getData();
 
-            return $this->redirectToRoute('search_list', [
+            return $this->redirectToRoute('app_search_list', [
                 'location' => $location,
             ]);
         }
 
         return $this->renderForm('search/index.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/search/{location}", name="app_search_list")
+     */
+    public function list(SpaceRepository $spaceRepository, string $location): Response
+    {
+
+        $space = $spaceRepository->findByLocation($location);
+        return $this->render('search/list.html.twig', [
+            'space' => $space,
         ]);
     }
 }
