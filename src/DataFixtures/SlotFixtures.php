3a32b35a0999c19ecc5458771136d05057e5d5c2
Noelie <noelie.dubos@gmail.com>
2021-12-08 16:53:48 +0100

    i add the SlotFixtures

--- src/DataFixtures/SlotFixtures.php
+++ src/DataFixtures/SlotFixtures.php
@@ -2,16 +2,42 @@
 
 namespace App\DataFixtures;
 
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Slot;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class SlotFixtures extends Fixture
+class SlotFixtures extends Fixture implements DependentFixtureInterface
 {
+
+    private const SPACE_COUNT = 30;
+    private const TIMESTAMP_START = 1638316800;
+    private const TIMESTAMP_END = 1638921600;
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= self::SPACE_COUNT; $i++) {
+            $slot = new Slot();
+            $slot->setPrice((rand(1, 10) / 10) * rand(100, 200));
+            $timestamp = rand(self::TIMESTAMP_START, self::TIMESTAMP_END);
+            $slot->setSlotTime(
+                new DateTime(
+                    date("Y-m-d", $timestamp)
+                )
+            );
+            $slot->setSpace($this->getReference('Espace ' . rand(1, 30)));
+
+            $manager->persist($slot);
+        }
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            SpaceFixtures::class,
+        ];
     }
 }
