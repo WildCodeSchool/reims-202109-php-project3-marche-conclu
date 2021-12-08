ffd379abd6dd3dcf379a6e6733b631b8f90b9b4c
Sayn <marvincrepin@gmail.com>
2021-12-08 15:59:57 +0100

    Removed SearchController and update SpaceController

--- src/Controller/SpaceController.php
+++ src/Controller/SpaceController.php
@@ -13,11 +13,34 @@ class SpaceController extends AbstractController
 {
 
     /**
-     * @Route("/space/{location}", name="app_space_list")
+     * @Route("/", name="app_home")
+     */
+    public function search(Request $request): Response
+    {
+
+        $form = $this->createForm(SearchType::class, null, array('method' => 'GET'));
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $location = $form->get('location')->getData();
+
+            return $this->redirectToRoute('app_space_list', [
+                'location' => $location,
+            ]);
+        }
+
+        return $this->renderForm('search/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/search", name="app_space_list")
      */
 
-    public function list(SpaceRepository $spaceRepository, string $location): Response
+    public function list(SpaceRepository $spaceRepository, Request $request): Response
     {
+        $location = $request->get('location');
 
         $space = $spaceRepository->findByLocation($location);
         return $this->render('search/list.html.twig', [
