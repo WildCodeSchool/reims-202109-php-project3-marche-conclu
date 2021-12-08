76ae02939b13ab5168236ae845b10ecd5f933123
Sayn <marvincrepin@gmail.com>
2021-12-08 10:33:09 +0100

    Database : Creation of Slot & Space entities

---
+++ src/Entity/Space.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SpaceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SpaceRepository::class)
+ */
+class Space
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private string $photos;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private string $surface;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private string $category;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private int $capacity;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private string $location;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Slot::class, mappedBy="space", orphanRemoval=true)
+     */
+    private Collection $slots;
+
+    public function __construct()
+    {
+        $this->slots = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPhotos(): ?string
+    {
+        return $this->photos;
+    }
+
+    public function setPhotos(string $photos): self
+    {
+        $this->photos = $photos;
+
+        return $this;
+    }
+
+    public function getSurface(): ?string
+    {
+        return $this->surface;
+    }
+
+    public function setSurface(string $surface): self
+    {
+        $this->surface = $surface;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(int $capacity): self
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Slot[]
+     */
+    public function getSlots(): Collection
+    {
+        return $this->slots;
+    }
+
+    public function addSlot(Slot $slot): self
+    {
+        if (!$this->slots->contains($slot)) {
+            $this->slots[] = $slot;
+            $slot->setSpace($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSlot(Slot $slot): self
+    {
+        if ($this->slots->removeElement($slot)) {
+            // set the owning side to null (unless already changed)
+            if ($slot->getSpace() === $this) {
+                $slot->setSpace(null);
+            }
+        }
+
+        return $this;
+    }
+}
