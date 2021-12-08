fae4ae027afa9725654cc39fa3193891a28d6ad8
Sayn <marvincrepin@gmail.com>
2021-12-08 17:40:55 +0100

    Navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,7 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
+
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
