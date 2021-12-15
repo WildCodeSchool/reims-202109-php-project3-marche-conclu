fd84a74cbbfc6c6d220c5203cfa91f96bde25f57
N26N26N26 <jordan.verreaux@gmail.com>
2021-12-15 12:17:17 +0100

    add space name

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-import './carrousel';
+import './carousel';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
