aae9c9d60ce5507df6da88d1e94e1c84030efeb9
N26N26N26 <jordan.verreaux@gmail.com>
2021-12-15 14:15:56 +0100

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import './bootstrap';
 import './carousel';
 
 // any CSS you import will output into a single css file (app.css in this case)
