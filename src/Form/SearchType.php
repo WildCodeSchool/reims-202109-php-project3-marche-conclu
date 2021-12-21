9fbcf0aa48db82a33be63a4bdc1114052ac5f42c
MarvinCrepin <82387640+MarvinCrepin@users.noreply.github.com>
2021-12-21 14:42:31 +0100

    Removed location list for a text input

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -8,20 +8,14 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class SearchType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('location', ChoiceType::class, [
-                'choices'  => [
-                    'Paris' => 'Paris',
-                    'Marseille' => 'Marseille',
-                    'Lyon' => 'Lyon',
-                ],
-                'label' => false,
-            ]);
+            ->add('location', TextType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
