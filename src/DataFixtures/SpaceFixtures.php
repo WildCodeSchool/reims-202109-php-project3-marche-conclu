fbc09b3535e1f1b6dea3844d52bc5d6ecedc5005
Xavier CHAVAUDREY <chavaudreyxav@hotmail.fr>
2022-01-05 10:48:45 +0100

    UserFixtures creation

--- src/DataFixtures/SpaceFixtures.php
+++ src/DataFixtures/SpaceFixtures.php
@@ -2,11 +2,12 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
 use App\Entity\Space;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class SpaceFixtures extends Fixture
+class SpaceFixtures extends Fixture implements DependentFixtureInterface
 {
     private const TYPESPACE = ['Bureau privé', 'Co-Working', 'Salle de réunion', 'Open Space', 'Espace de stockage'];
     private const LOCATION = ['Reims', 'Charleville', 'Lyon', 'Paris', 'Epernay'];
@@ -30,9 +31,17 @@ class SpaceFixtures extends Fixture
             $location = array_rand(self::LOCATION);
             $space->setLocation(self::LOCATION[$location]);
             $this->addReference($space->getName(), $space);
+            $space->setOwner($this->getReference('user_David@email.com'));
             $manager->persist($space);
         }
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UserFixtures::class,
+        ];
     }
 }
