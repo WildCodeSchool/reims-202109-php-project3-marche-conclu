6ce1eef587dce96b4f92b336f82a0277b6742829
N26N26N26 <jordan.verreaux@gmail.com>
2022-01-12 18:43:55 +0100

    added frontend secure in order to don't let the possibility to reserve a date before tomorrow

--- src/Form/SlotType.php
+++ src/Form/SlotType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Slot;
+use Datetime;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,6 +19,9 @@ class SlotType extends AbstractType
                 'widget' => 'single_text',
                 // this is actually the default format for single_text
                 'format' => 'yyyy-MM-dd',
+                'attr' =>  array(
+                    'min' => (new DateTime('+1 day'))->format('Y-m-d'),
+                )
             ]);
     }
 
