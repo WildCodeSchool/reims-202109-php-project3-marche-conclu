bc56a488ac385ce7a1cfe91dd7cc1e765282f4a4
N26N26N26 <jordan.verreaux@gmail.com>
2022-01-13 16:25:19 +0100

    refont the show user

---
+++ migrations/Version20220113134127.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220113134127 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE space ADD address VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD phone_number VARCHAR(15) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE space DROP address');
+        $this->addSql('ALTER TABLE user DROP phone_number');
+    }
+}
