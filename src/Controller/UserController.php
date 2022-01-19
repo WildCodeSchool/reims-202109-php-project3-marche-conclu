7346232b7ec27fdd08b1dcb2ae29c1d9cbc1403d
N26N26N26 <jordan.verreaux@gmail.com>
2022-01-19 17:15:11 +0100

    added link to access of the user public page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -23,9 +23,12 @@ class UserController extends AbstractController
     * @IsGranted("ROLE_USER")
     */
     #[Route('/profile', name: 'user_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig');
+        $user = $userRepository->findAll();
+        return $this->render('user/index.html.twig', [
+            'user' => $user,
+        ]);
     }
 
     /**
