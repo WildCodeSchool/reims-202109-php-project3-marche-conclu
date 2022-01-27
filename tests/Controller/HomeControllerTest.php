dc75ad85091f4f0eee0e9fdaed71bbbe6469b525
Xavier CHAVAUDREY <chavaudreyxav@hotmail.fr>
2022-01-27 09:54:45 +0100

    test phpUnit

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Controller\HomeController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testIndexExists(): void
+    {
+        $homeController = new HomeController();
+        $this->assertTrue(
+            method_exists($homeController, 'index')
+        );
+    }
+}
