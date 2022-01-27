3cb24e050f4ebac0257715e377738913c3f4b63a
Xavier CHAVAUDREY <chavaudreyxav@hotmail.fr>
2022-01-27 17:02:35 +0100

    fixture with disponibility

--- migrations/Version20220127155636.php
+++ migrations/Version20220127155636.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220127142950 extends AbstractMigration
+final class Version20220127155636 extends AbstractMigration
 {
     public function getDescription(): string
     {
