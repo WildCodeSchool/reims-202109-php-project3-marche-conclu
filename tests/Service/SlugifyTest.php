e6c9bc52006e4d395f7caeaeb3159c2656328710
Xavier CHAVAUDREY <chavaudreyxav@hotmail.fr>
2022-01-27 20:33:39 +0100

    install Test Unit

---
+++ tests/Service/SlugifyTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Service;
+
+use App\Service\Slugify;
+use PHPUnit\Framework\TestCase;
+
+class SlugifyTest extends TestCase
+{
+   /**
+    * @dataProvider getSlugs
+    */
+    public function testAssignSlug(string $firstString, string $lastString, string $slug): void
+    {
+        $slugify = new Slugify();
+        $this->assertSame($slug, $slugify->assignSlug($firstString, $lastString));
+    }
+
+    public function getSlugs(): array
+    {
+        return [
+            ['Lorem', 'Ipsum', 'loremipsum'],
+            ['  Lorem', 'Ipsum  ', 'loremipsum'],
+            [' lOrEm ', 'iPsUm  ', 'loremipsum'],
+            ['!Lorem', 'Ipsum!', 'loremipsum'],
+            ['lorem-ipsum', '', 'lorem-ipsum'],
+            ['lorem 日本語', 'ipsum', 'lorem日本語ipsum'],
+            ['lorem русский язык', 'ipsum', 'loremрусскийязыкipsum'],
+        ];
+    }
+}
