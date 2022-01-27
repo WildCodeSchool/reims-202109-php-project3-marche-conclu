55ac6f5c41788132410d244d1d81daa0f2a2058e
Noelie <noelie.dubos@gmail.com>
2022-01-27 17:31:57 +0100

    i align some text

--- migrations/Version20220127144520.php
+++ migrations/Version20220127144520.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220126181556 extends AbstractMigration
+final class Version20220127144520 extends AbstractMigration
 {
     public function getDescription(): string
     {
